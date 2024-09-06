735d3c0653bdaad5c9fddcbe0f0dabc7ad49d608
Sheetal Naik <sheetalnaik848@gmail.com>
2024-09-06 17:55:12 +0530

    search option

---
+++ countries-api-react-search/src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import SearchBar from './components/SearchBar'
+import SelectMenu from './components/SelectMenu'
+import CountriesList from './components/CountriesList'
+
+const App = () => {
+
+  const [query, setQuery] = useState('');
+
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="search-filter-container">
+          <SearchBar setQuery={setQuery}/>
+          <SelectMenu />
+        </div>
+        <CountriesList query={query}/>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ countries-api-react-search/src/components/CountriesList.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import countriesData from '../countriesData'
+import CountryCard from './CountryCard'
+
+const CountriesList = ({query}) => {
+  return (
+    <>
+    <div className="countries-container">
+      {countriesData.filter((country) => country.name.common.toLowerCase().includes(query))
+      .map((country) => {
+        return (
+          //console.log(country)
+          <CountryCard 
+          key={country.name.common}
+          name={country.name.common}
+          flag={country.flags.svg}
+          population={country.population}
+          region={country.region}
+          capital={country.capital?.[0]}
+          />
+        )
+      })}
+    </div>
+    </>
+  )
+}
+
+export default CountriesList
